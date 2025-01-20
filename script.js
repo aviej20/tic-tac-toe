9f86dd00c6c9471ca4f0158abfb485e6ba30a3cd
aviej20 <90211835+aviej20@users.noreply.github.com>
2025-01-20 14:20:27 -0600

    Created playable tic tac toe js in console

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const createGameBoard = (function (){
+function Cell(){
+
+    marker = "";
+
+    return {marker};
+}
+
+const gameBoard = (function (){
 
     const rows = 3, columns = 3;
     const board = [];
@@ -11,16 +18,108 @@ const createGameBoard = (function (){
         }
     }
 
-    console.log(board);
-    return {};
+    const getBoard = () => board;
+
+
+    const notEmptyBoard = () => {
+            return board.every(rows => rows.every(columns => columns.marker !== ""));
+        };
+
+    const addMarker = (activePlayer, row, column) => {
+
+            if(!board[row][column].marker){
+                board[row][column].marker = activePlayer.marker;
+                board[row][column].playerSpace = activePlayer.name;
+                console.log(board[row][column]);
+            }
+            else{
+                console.log("Not a valid space");
+            }
+        
+    };
+
+
+    const printBoard = () => {
+
+        const newBoard = board.map((rows) => {rows.map((column)=>{ return column.marker}); return rows}); // column returns object array to rows array 
+        console.log(newBoard);
+
+    };
+
+
+    return {getBoard, addMarker, printBoard, notEmptyBoard};
 })();
 
+const gameMaster = (function (){
+    
+    let playerOneName = "Player One";
+    let playerTwoName = "Player Two"
 
-function Cell(){
+    const players = [
+        {
+            name: playerOneName,
+            marker: "X"
+        },
+        {
+            name: playerTwoName,
+            marker: "O"
+        }
+    ];
 
-    let playerSpace = "", marker = "";
+    let activePlayer = players[0];
 
+    const getActivePlayer = () => activePlayer;
 
+    const playRound = (inputRow, inputColumn) =>{
+        console.log("New Round");
+        gameBoard.addMarker(getActivePlayer(), inputRow, inputColumn);
+        gameBoard.printBoard();
+    };
 
-    return {playerSpace, marker};
-}
+    const switchPlayerTurn = () => {
+        console.log("Switching Players...");
+        activePlayer = (activePlayer == players[0]) ? players[1] : players[0];
+    };
+
+    const gameWinner = () =>{
+    
+        const currentBoard = gameBoard.getBoard();
+
+        let i =0;
+        while(i < currentBoard.length){
+            //check rows
+            if ((currentBoard[i][0].marker !== "") && (currentBoard[i][0].marker == currentBoard[i][1].marker) && (currentBoard[i][1].marker == currentBoard[i][2].marker)){
+                console.log("Winner through rows");
+                return true;
+            }
+            //check columns
+            else if ((currentBoard[0][i].marker !== "") && (currentBoard[0][i].marker == currentBoard[1][i].marker) && (currentBoard[1][i].marker == currentBoard[2][i].marker)){
+                console.log("Winner through columns");
+                return true;
+            }
+            //check diagonals
+            else if ((currentBoard[0][0].marker !== "") && (currentBoard[0][0].marker == currentBoard[1][1].marker) && (currentBoard[1][1].marker == currentBoard[2][2].marker)){
+                console.log("Winner through diagonals");
+                return true;
+            }
+            else if ((currentBoard[2][0].marker !== "") && (currentBoard[2][0].marker == currentBoard[1][1].marker) && (currentBoard[1][1].marker == currentBoard[0][2].marker)){
+                console.log("Winner through diagonals");
+                return true;
+            }
+            //check tie
+            else if (gameBoard.notEmptyBoard() == true){
+                console.log("Tie game");
+                return true;
+            }
+            i++;
+        }
+    };
+
+   while(!gameWinner()){
+        getActivePlayer();
+        var inputRow = prompt(`${getActivePlayer().name} Insert Row`), inputColumn = prompt(`${getActivePlayer().name} Insert Column`);
+        console.log("Row: " + inputRow + " Column: " + inputColumn);
+        playRound(inputRow, inputColumn);
+        switchPlayerTurn();
+   } 
+})();
