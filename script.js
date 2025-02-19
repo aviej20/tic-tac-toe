2828c01fba3c038dd2d7da9deca0a79ef806a1d1
aviej20 <90211835+aviej20@users.noreply.github.com>
2025-02-18 19:21:00 -0600

    Finished tic tac toe game logic with working interface

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function Cell(){
+function Cell(row, column){
 
+    rowNum = row;
+    columnNum = column;
     marker = "";
 
-    return {marker};
+    return {marker, rowNum, columnNum};
 }
 
+/* Creates gameboard as well as contains notEmptyBoard, addMarker to cell, printBoard, & getBoard
+    Returns parameters for an object containing the above function
+ */
 const gameBoard = (function (){
 
     const rows = 3, columns = 3;
@@ -14,7 +19,7 @@ const gameBoard = (function (){
         board[i] = [];
 
         for(let j=0; j< columns; j++){
-            board[i].push(Cell());
+            board[i].push(Cell(i, j));
         }
     }
 
@@ -22,18 +27,19 @@ const gameBoard = (function (){
 
 
     const notEmptyBoard = () => {
-            return board.every(rows => rows.every(columns => columns.marker !== ""));
+            return board.every(row => row.every(cell => cell.marker !== ""));
         };
 
     const addMarker = (activePlayer, row, column) => {
 
             if(!board[row][column].marker){
                 board[row][column].marker = activePlayer.marker;
-                board[row][column].playerSpace = activePlayer.name;
                 console.log(board[row][column]);
+                return true;
             }
             else{
                 console.log("Not a valid space");
+                return false;
             }
         
     };
@@ -41,13 +47,31 @@ const gameBoard = (function (){
 
     const printBoard = () => {
 
-        const newBoard = board.map((rows) => {rows.map((column)=>{ return column.marker}); return rows}); // column returns object array to rows array 
+        const newBoard = board.map(row => row.map(cell => cell.marker)); // cell returns object array to row array 
         console.log(newBoard);
 
     };
 
 
-    return {getBoard, addMarker, printBoard, notEmptyBoard};
+    const resetBoard = () => {
+
+    let row = 0;
+    while(row < 3){
+        for(let column = 0; column < 3; column++){
+
+            const cell = document.querySelector(`.cell[data-row="${row}"][data-column="${column}"]`);
+
+            const img = cell.querySelector("img");
+
+            img.classList.replace("unhidden-svg", "hidden-svg");
+        }
+        row++;
+    }
+    board.map(row => row.map(cell => cell.marker = ""));
+    console.log(printBoard());
+}
+
+    return {getBoard, addMarker, printBoard, notEmptyBoard, resetBoard};
 })();
 
 const gameMaster = (function (){
@@ -70,12 +94,6 @@ const gameMaster = (function (){
 
     const getActivePlayer = () => activePlayer;
 
-    const playRound = (inputRow, inputColumn) =>{
-        console.log("New Round");
-        gameBoard.addMarker(getActivePlayer(), inputRow, inputColumn);
-        gameBoard.printBoard();
-    };
-
     const switchPlayerTurn = () => {
         console.log("Switching Players...");
         activePlayer = (activePlayer == players[0]) ? players[1] : players[0];
@@ -86,32 +104,39 @@ const gameMaster = (function (){
         const currentBoard = gameBoard.getBoard();
 
         let i =0;
-        while(i < currentBoard.length){
+        while(i < 3){
             //check rows
             if ((currentBoard[i][0].marker !== "") && (currentBoard[i][0].marker == currentBoard[i][1].marker) && (currentBoard[i][1].marker == currentBoard[i][2].marker)){
                 console.log("Winner through rows");
+                alert("Winner through rows");
                 return true;
             }
             //check columns
             else if ((currentBoard[0][i].marker !== "") && (currentBoard[0][i].marker == currentBoard[1][i].marker) && (currentBoard[1][i].marker == currentBoard[2][i].marker)){
                 console.log("Winner through columns");
-                return true;
-            }
-            //check diagonals
-            else if ((currentBoard[0][0].marker !== "") && (currentBoard[0][0].marker == currentBoard[1][1].marker) && (currentBoard[1][1].marker == currentBoard[2][2].marker)){
-                console.log("Winner through diagonals");
-                return true;
-            }
-            else if ((currentBoard[2][0].marker !== "") && (currentBoard[2][0].marker == currentBoard[1][1].marker) && (currentBoard[1][1].marker == currentBoard[0][2].marker)){
-                console.log("Winner through diagonals");
-                return true;
-            }
-            //check tie
-            else if (gameBoard.notEmptyBoard() == true){
-                console.log("Tie game");
+                alert("Winner through columns");
                 return true;
             }
             i++;
+        }
+
+             //check diagonals
+         if ((currentBoard[0][0].marker !== "") && (currentBoard[0][0].marker == currentBoard[1][1].marker) && (currentBoard[1][1].marker == currentBoard[2][2].marker)){
+                console.log("Winner through diagonals");
+                alert("Winner through diagonals");
+                return true;
+        }
+        else if ((currentBoard[2][0].marker !== "") && (currentBoard[2][0].marker == currentBoard[1][1].marker) && (currentBoard[1][1].marker == currentBoard[0][2].marker)){
+                console.log("Winner through diagonals");
+                alert("Winner through diagonals");
+                return true;
+        }
+
+        //check tie
+        if (gameBoard.notEmptyBoard() == true){
+            console.log("Tie game");
+            alert("Tie game");
+            return true;
         }
     };
 
@@ -121,54 +146,151 @@ const gameMaster = (function (){
         return {choiceRow, choiceColumn};
     };
 
-   while(!gameWinner()){
+    const playRound = (inputRow, inputColumn) => {
+    
+        console.log("New Round");
+
         let currentPlayer = getActivePlayer();
-        /*var inputRow = prompt(`${getActivePlayer().name} Insert Row`), inputColumn = prompt(`${getActivePlayer().name} Insert Column`);
-        console.log("Row: " + inputRow + " Column: " + inputColumn);*/
 
-        if(currentPlayer.name == "Player Two"){
-            let compChoice = getCompChoice();
-            playRound(compChoice.choiceRow, compChoice.choiceColumn);
-        }else{
-            let playerCHoice;
+        console.log("Current Player: " + currentPlayer.name);
+
+        // Player's turn
+        if (currentPlayer.name === "Player One") {
+            console.log("Person going...");
+    
+            let playersMove = gameBoard.addMarker(currentPlayer, inputRow, inputColumn);
+            // Check if move is valid before continuing
+            if (!playersMove) {
+                alert("Space is full, Please try again");
+                console.log("Space is full");
+                return;
+            }
+
+            gameBoard.printBoard();
+    
+
+            if (gameWinner()) {
+                console.log("Game Over");
+                return;
+            }
+        
+            switchPlayerTurn();
         }
-        switchPlayerTurn();
-   } 
 
-    return{getActivePlayer};
+       currentPlayer = getActivePlayer();
+
+
+        if (currentPlayer.name === "Player Two") {
+            console.log("Computer going...");
+    
+            let compChoice;
+            let validMove = false;
+    
+            while (!validMove) {
+                compChoice = getCompChoice();
+                validMove = gameBoard.addMarker(currentPlayer, compChoice.choiceRow, compChoice.choiceColumn);
+            }
+    
+            screenDOMController.updateCellImage(currentPlayer, compChoice.choiceRow, compChoice.choiceColumn);
+            gameBoard.printBoard();
+    
+            if (gameWinner()) {
+                console.log("Game Over");
+                return;
+            }
+            switchPlayerTurn();
+        }
+
+        console.log("current player: " + currentPlayer.name);
+     
+    };
+
+    return{playRound, getActivePlayer, gameWinner};
 })();
 
-function screenDOMController(){
+
+const screenDOMController= (function(){
+
+
+    const nameEnteringPrompt = () => {
+
+        const form = document.createElement("form");
+
+        //form.appendChild()
+
+    };
+
+    const getPlayerChoice = (row, column) => { 
+
+        console.log("Clicked column: " + column);
+
+        return{row, column};
+    };
+
+    
+    const updateCellImage = (activePlayer, row, column) => {
+
+        const cell = document.querySelector(`[data-row="${row}"][data-column="${column}"]`);
+
+        const img = cell ? cell.querySelector("img") : "";
+
+        if(activePlayer.marker == "O"){
+            img.setAttribute("src", "svgs/skull.svg");
+            img.classList.replace("hidden-svg", "unhidden-svg");
+        }
+        else{
+            img.classList.replace("hidden-svg", "unhidden-svg");
+        }
+
+    };
 
     const gameBoardContainer = document.getElementsByClassName("gameboard-container");
 
-    for(let i=0; i < 9; i++){
+    const board = gameBoard.getBoard();
+
+    board.forEach(row => { row.forEach(column => {
 
         const gameBoardCell = document.createElement("div");
+
+        gameBoardCell.dataset.row = column.rowNum;
+        gameBoardCell.dataset.column =column.columnNum;
 
         const image = document.createElement("img");
 
         image.classList.add("svg-styles", "hidden-svg");
 
-        let activePlayer = gameMaster.getActivePlayer();
-
-        if(activePlayer.name == "Player One" ){
-
-            image.setAttribute("src", "svgs/crossbones.svg");
-        }
-        else{
-            image.setAttribute("src", "svgs/skull.svg");
-        }
+        image.setAttribute("src", "svgs/crossbones.svg"); //placeholder
 
         gameBoardCell.addEventListener("click", ()=>{
-                 image.classList.replace("hidden-svg", "unhidden-svg")
+
+                if(gameMaster.gameWinner()){
+                    return;
+                }
+                else{
+                const {row, column} = getPlayerChoice(gameBoardCell.dataset.row, gameBoardCell.dataset.column);
+
+                updateCellImage(gameMaster.getActivePlayer(), row, column);
+
+                 gameMaster.playRound(row, column);
+                }
                 });
 
         gameBoardCell.classList.add("cell");
 
         gameBoardContainer[0].appendChild(gameBoardCell).appendChild(image);
-    }
+    })
+    });
 
+    document.getElementById('reset').addEventListener("click", ()=> {
+        
+        console.log("Reset button worked");
+        gameBoard.resetBoard();
 
+        if(gameMaster.getActivePlayer().name == "Player Two"){
+            gameMaster.switchPlayerTurn();
+        }
+    });
 
-    } screenDOMController();
+    return {updateCellImage};
+
+})();
